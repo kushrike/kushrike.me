803dd914d57be0d3267192f3c077d109ece89daa
Kushal Srivastava <kushal.srivastava@captainfresh.in>
2023-10-23 18:41:46 +0530

    Social links open in new tab

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -43,7 +43,7 @@ const data = [
 const links = data.map(link => {
   return (
     <li key={link.id}>
-      <a href={link.url} className="social-link">
+      <a href={link.url} className="social-link" target="_blank">
         {link.icon}
       </a>
     </li>
